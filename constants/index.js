ce94ab9e034a147c9e9cf7d1c63c53640b33b7e1
hamedlak <141082198+hamedlak@users.noreply.github.com>
2023-08-01 02:26:54 +0330

    Update index.js
    update contract

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xdfe21578c709124f6d5c8785ae063c66b65a334b";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
